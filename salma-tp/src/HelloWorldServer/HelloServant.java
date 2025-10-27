86ece068decd85d1d26c83d2637351d50cdf72df
hajar <hajarlamghari0000@gmail.com>
2025-10-27 16:00:05 +0100

    les fichiers du tp

---
+++ salma-tp/src/HelloWorldServer/HelloServant.java
@@ -0,0 +1,20 @@
+package HelloWorldServer; 
+import org.omg.CORBA.ORB; 
+import HelloWorldApp.HelloPOA;
+  public class HelloServant extends HelloPOA {
+  private String message= "Bonjour tous Le monde !!"; private ORB orb;
+ 
+  public void setOrb(ORB orb) { this.orb = orb;
+  }
+ 
+  @Override
+  public String HelloMessage() {
+	  return message;
+  }
+ 
+
+  @Override
+ public void HelloMessage(String newHelloMessage) { message = newHelloMessage;
+ }
+ 
+  }
